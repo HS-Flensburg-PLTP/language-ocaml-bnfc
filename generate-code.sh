0fdf775135dce1bb447bd4ff23c23cbab3e6feb9
Jan Christiansen <jan.christiansen@hs-flensburg.de>
2024-07-15 08:43:00 +0200

    Generate instances of generic classes

--- generate-code.sh
+++ generate-code.sh
@@ -1,6 +1,6 @@
 set -euxo pipefail
 
-bnfc --haskell -d -p Language -o src -m ocaml.cf
+bnfc --haskell -d -p Language --generic -o src -m ocaml.cf
 patch src/Language/Ocaml/Lex.x < Lex.x.patch
 alex --ghc src/Language/Ocaml/Lex.x
 happy --ghc --coerce --array --info src/Language/Ocaml/Par.y
