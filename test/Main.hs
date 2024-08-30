9db0bd32333081f6ad9565ea208c7361dce555ba
strasper <struckjasper@gmail.com>
2024-08-30 10:26:48 +0200

    Read input from files

--- test/Main.hs
+++ test/Main.hs
@@ -1,20 +1,24 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 import Language.Ocaml.Par (myLexer, pImplementation)
+import Path (fromRelDir, reldir)
+import System.FilePath (takeFileName)
+import System.FilePath.Find (always, extension, fileName, find, (==?))
 import Test.HUnit
 
-successTest :: String -> Test
-successTest input = TestCase $ do
+successTest :: FilePath -> Test
+successTest filePath = TestCase $ do
+  input <- readFile filePath
   let ts = myLexer input
   case pImplementation ts of
     Right _ -> return ()
-    Left err -> assertFailure ("Parsing failed with error: " ++ err)
+    Left err -> assertFailure ("Parsing " ++ fileName ++ " failed with error: " ++ err)
+  where
+    fileName = takeFileName filePath
 
 main :: IO Counts
-main =
+main = do
+  filePaths <- find always (extension ==? ".ml") (fromRelDir [reldir|test/programs|])
   runTestTT $
     TestList
-      ( map
-          successTest
-          [ "x = '",
-            "let x =\"Usage: %s [COMMAND] \n\nValid values for COMMAND:\n%s\n\nDefault values if unspecified:\n COMMAND\tstatus\n\nStatus command options:\""
-          ]
-      )
+      (map successTest filePaths)
